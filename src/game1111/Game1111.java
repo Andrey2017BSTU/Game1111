d582c70deced1ea6dd5df018fdb3edf9fe2d2794
andrey <andrey@andrey-Lenovo-ideapad-100-15IBD>
2017-12-24 02:11:41 +0300

    Fix b.0.3

--- src/game1111/Game1111.java
+++ src/game1111/Game1111.java
@@ -5,9 +5,10 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 class Field{
-    static int mas[][] = new int [10][10];
-    void inField(int obs){
-        int k = 0;
+    static int  mas[][] = new int [10][10];//написать заполнение массива нулями 
+     
+    void inField(int obs){//и присваивать их индексы переменными что б
+        int k = 0;// потом обращяться к ним чере f. а не h.
         while(k<obs){
             mas[(int)(Math.random()*10)][(int)(Math.random()*10)]=1;
             if (mas[9][0] != 1 && mas[0][9] != 1) {  
@@ -29,14 +30,20 @@ class Hero{
     int x=0, y=9, step = 0;
     void hh(){
         Field.mas[y][x]=2;
+        
     }
     
     
 }
 
 class Game{
+
+    Game() {
+
+    }
     private int getDirection(){
         int direction = 0;
+        
 
         try {
             BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -51,14 +58,14 @@ class Game{
     Hero h = new Hero();
     Field f = new Field();
     
-    
-    void move(){
+    void move(){  int y1, y2, x1, x2;  
         switch(getDirection()){
-            case 8: h.y--; h.y++; Field.mas[h.x][h.y]=0;h.y-- ;break;/// Тут менял, нужно попробовать  
-            case 2: h.y++; break;/////добавить цикл в gameField
-            case 4: h.x--; break;/////или Startgame что бы поле обновлялось после каждого хода
-            case 6: h.x++; break;
+            case 8: y1=h.y; h.y--; Field.mas[y1][h.x]=0; break ;///добил идею свою наконец 
+            case 2: y2=h.y; h.y++; Field.mas[y2][h.x]=0; break;// блять ебался с этим пока наконец не понял,
+            case 4: x1=h.x; h.x--; Field.mas[h.y][x1]=0; break;///что ты массив задал через y;x ,а не 
+            case 6: x2=h.x; h.x++; Field.mas[h.y][x2]=0; break;// через x;y 
             default: System.out.println("Неверное направление!");
+        
         }
        // System.out.println(h.x);
     }
@@ -69,6 +76,7 @@ class Game{
             h.hh();
             f.printField();
             move(); 
+            
         }
     }
     
